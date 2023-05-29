f4c7fa7518b9384b25c1191ac81e71963631b2f3
Mateus V. Gomes <monkmitogames@hotmail.com>
2023-05-29 17:34:09 +0000

    Friend com ID inserido algumas logicas modificadas

--- scripts/friendBox.js
+++ scripts/friendBox.js
@@ -1,6 +1,48 @@
 const profileBox = document.getElementById("profileBox");
 
-let countOfFriendsPerPage = 1;
+let countOfFriendsPerPage;
+fetch("https://reqres.in/api/users?page=2", {
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+  .then((response) => response.json())
+  .then((processedData) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(processedData);
+      }, 1200);
+    });
+  })
+  .then((processedData) => {
+    countOfFriendsPerPage = 1;
+    setTimeout(() => {
+      while (countOfFriendsPerPage < 7) {
+        const profilePicutre = document.createElement("img");
+        let imageUrl = processedData.data[countOfFriendsPerPage - 1].avatar;
+        setImageUrl(profilePicutre, imageUrl);
+        const friendName = document.createElement("p");
+        setFriendName(friendName, processedData);
+        const friendId = document.createElement("p");
+        console.log(friendId.textContent);
+        setFriendId(friendId, processedData);
+        const friendBox = document.createElement("section");
+        createSection(
+          friendBox,
+          profilePicutre,
+          friendName,
+          processedData.data[countOfFriendsPerPage - 1].id,
+          friendId
+        );
+        countOfFriendsPerPage++;
+      }
+    }, 120);
+    countOfFriendsPerPage = 1;
+  })
+  .catch((error) => {
+    console.log("Erro:", error);
+  });
+
 fetch("https://reqres.in/api/users?page=1", {
   headers: {
     "Content-Type": "application/json",
@@ -8,15 +50,26 @@ fetch("https://reqres.in/api/users?page=1", {
 })
   .then((response) => response.json())
   .then((processedData) => {
+    countOfFriendsPerPage = 1;
+    console.log(processedData.data[1].avatar);
     setTimeout(() => {
       while (countOfFriendsPerPage < 7) {
         const profilePicutre = document.createElement("img");
         let imageUrl = processedData.data[countOfFriendsPerPage - 1].avatar;
         setImageUrl(profilePicutre, imageUrl);
-        const friendName=document.createElement('p');
-        setFriendName(friendName,processedData);
+        const friendName = document.createElement("p");
+        setFriendName(friendName, processedData);
+        const friendId = document.createElement("p");
+        console.log(friendId.textContent);
+        setFriendId(friendId, processedData);
         const friendBox = document.createElement("section");
-        createSection(friendBox, profilePicutre,friendName);
+        createSection(
+          friendBox,
+          profilePicutre,
+          friendName,
+          processedData.data[countOfFriendsPerPage - 1].id,
+          friendId
+        );
         countOfFriendsPerPage++;
       }
     }, 120);
@@ -25,17 +78,26 @@ fetch("https://reqres.in/api/users?page=1", {
     console.log("Erro:", error);
   });
 
-  function setFriendName(friendName,processedData){
-      friendName.textContent=`${processedData.data[countOfFriendsPerPage - 1].first_name} ${processedData.data[countOfFriendsPerPage - 1].last_name}`
-  }
-  function setImageUrl(profilePicture, imageUrl) {
+function setFriendId(friendId, processedData) {
+  friendId.textContent = `ID:${
+    processedData.data[countOfFriendsPerPage - 1].id
+  } `;
+}
+
+function setFriendName(friendName, processedData) {
+  friendName.textContent = `${
+    processedData.data[countOfFriendsPerPage - 1].first_name
+  } ${processedData.data[countOfFriendsPerPage - 1].last_name}`;
+}
+function setImageUrl(profilePicture, imageUrl) {
   profilePicture.src = imageUrl;
 }
-function createSection(friendBox, profilePicutre,friendName) {
-  friendBox.className = "friend";
-  friendBox.id = `friendBox-${countOfFriendsPerPage}`;
+function createSection(friendBox, profilePicutre, friendName, id, friendId) {
+  friendBox.className = "friend container-fluid  col-lg-3 col-md-6 col-sm-6 ";
+  friendBox.id = `friendBox-${id}`;
   profileBox.appendChild(friendBox);
+
   friendBox.appendChild(profilePicutre);
   friendBox.appendChild(friendName);
-  
+  friendBox.appendChild(friendId);
 }
