3ed96dc8dce3a6f1b6f9dda68d61a82f0dd05e91
Mateus V. Gomes <monkmitogames@hotmail.com>
2023-05-30 22:03:04 +0000

    Remoção do amigo da pagina

--- scripts/removeFriend.js
+++ scripts/removeFriend.js
@@ -2,17 +2,26 @@ const removeButton = document.getElementById("removeButton");
 let itemToBeRemoved;
 removeButton.addEventListener("click", () => {
   itemToBeRemoved = document.getElementById("toBeRemovedInput").value;
-  console.log(itemToBeRemoved);
-  fetch(`https://reqres.in/api/users?page=${itemToBeRemoved}`)
-  .then((response) => {
-    if (response.ok) {
-      console.log("Usuário excluído com sucesso");
-    } else {
-      console.log("Erro ao excluir o usuário");
-    }
-  })
-  .catch((error) => {
-    console.log("Erro:", error);
-  });
 
+  const elementToBeRemoved = document.getElementById(
+    `friendBox-${itemToBeRemoved}`
+  );
+  if (elementToBeRemoved != undefined) {
+    elementToBeRemoved.remove();
+
+    fetch(`https://reqres.in/api/users?page=${itemToBeRemoved}`)
+      .then((response) => {
+        if (response.ok) {
+          console.log("Usuário excluído com sucesso");
+        } else {
+          console.log("Erro ao excluir o usuário");
+        }
+      })
+      .catch((error) => {
+        console.log("Erro:", error);
+      });
+  }
+  else{
+    alert('Você não tem nenhum amigo com esta ID');
+  }
 });
